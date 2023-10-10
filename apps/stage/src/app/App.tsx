22ed002d25d3a16e3d040dcc07b82bc0adc0e922
codelocksdev <codelocksdev@gmail.com>
2023-10-10 07:58:31 -0500

    example

---
+++ apps/stage/src/app/App.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React from 'react';
+import { SafeAreaView, StatusBar, Text, } from 'react-native';
+import { Canvas, Circle, Group } from "@shopify/react-native-skia";
+
+export const App = () => {
+  const size = 256;
+  const r = size * 0.33;
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>Welcome to my reproducible example!</Text>
+        <Canvas style={{ flex: 1 }}>
+          <Group blendMode="multiply">
+            <Circle cx={r} cy={r} r={r} color="cyan" />
+            <Circle cx={size - r} cy={r} r={r} color="magenta" />
+            <Circle
+              cx={size/2}
+              cy={size - r}
+              r={r}
+              color="yellow"
+            />
+          </Group>
+        </Canvas>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
